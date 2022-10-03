2ce9f148a466daf7c11d0ae1bfa9984879693343
Kevin-Krantz <kevinkrantz95@hotmail.com>
2022-10-03 20:10:27 +0200

    fixed my stupid error

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -11,7 +11,7 @@ function Product({ _id, name, category, price, imgUrl }: IProduct) {
       </div>
       <div>
         <Text className="price">{price}</Text>
-        <Button>Köp denna nu!</Button>
+        <Button>Köp</Button>
       </div>
     </Container>
   );
